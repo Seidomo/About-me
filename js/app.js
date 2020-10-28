8125881d2f5d26092b5b88171fe2a17c8bd31dc4
Seid Mohamed <seid783@gmail.com>
2020-10-27 21:13:22 -0700

    about-me edited

--- js/app.js
+++ js/app.js
@@ -1,65 +1,88 @@
 
 
 
-'use strict'
+   'use strict'
 
-   var userName= prompt("please enter your name?");
-        //console.log();
+    var userName= prompt("please enter your name?");
+        //console.log('Greetings  ' + userName);
         alert('Greetings  ' + userName);
 
-    var myHeight= prompt('Hi ' + userName + ' how tall do yo think I am? ');
-       // console.log();
-       
-       
-        if(myHeight == 6.3) {
+    var aboutVegan= prompt('Hi ' + userName + ' do you think i am a vegan? ');
+      
+       if(aboutVegan === 'no') {
+            //console.log('yap you got right'); 
             alert('yap you got right');
         }
-        else{
-            alert ('Sorry no I am 6.3');
+        else if(aboutVegan .toLowerCase()=== 'yes'){
+            //console.log ('Sorry no am not a vegan ');
+            alert ('Sorry no am not a vegan ');
         } 
+        else{
+            //console.log('please answer yes or no');
+            alert('please answer yes or no');
+        }
         
-    var hairColor= prompt('Do you think my hair color is white ?');
+    var hairColor= prompt('Hi ' + userName + ' Do you think my hair color is white ?');
     
     
-        if( hairColor .toLowerCase == 'yes'){
-            console.log();
-            alert('No sorry its black')
+        if( hairColor .toLowerCase() === 'yes'){
+            //console.log('No sorry its black');
+            alert('No sorry its black');
 
+        }
+        else if(hairColor .toLowerCase() === 'no'){
+            //console.log ('You right its not please click ok to continue');
+            alert ('You right its not please click ok to continue');
         }
         else{
-            alert ('You right its not please click ok to continue')
+           // console.log('please answer yes or no');
+            alert('please answer yes or no');
         }
 
-    var favoriteColor= prompt('Do you think my favorite color is Blue ?');
+    var favoriteColor= prompt('Hi ' + userName + ' Do you think my favorite color is Blue ?');
     
-        if( favoriteColor .toLowerCase == 'yes'){
-            console.log();
-             alert ('Applause you got it please click ok to continue')
+        if( favoriteColor .toLowerCase() === 'yes'){
+             //console.log('Applause '  + userName + ' you got it please click ok to continue');
+             alert ('Applause '  + userName + ' you got it please click ok to continue');
+        }
+        else if (favoriteColor.toLowerCase() === 'no'){
+            //console.log('Sorry no its Blue');
+            alert ('Sorry no its Blue');
         }
         else{
-            alert ('Sorry no its Blue')
+            //console.log('please answer yes or no');
+            alert('please answer yes or no');
         }
 
-     var favoriteTeam= prompt('Is Seahawks my favorite team ?');
+     var favoriteTeam= prompt('Hi ' + userName + ' Is Seahawks my favorite team ?');
      
-        if( favoriteTeam .toLowerCase == 'yes'){
-            console.log();
-            alert('GO HAWKS you got it right')
+        if( favoriteTeam .toLowerCase() === 'yes'){
+            //console.log('GO HAWKS you got it right');
+            alert('GO HAWKS you got it right');
+        }
+        else if(favoriteTeam .toLowerCase() === 'no'){
+            //console.log( 'No i am a SEAHAWKS fan');
+            alert( 'No i am a SEAHAWKS fan');
         }
         else{
-            alert( 'No i am a SEAHAWKS fan')
+            //console.log('please answer yes or no');
+            alert('please answer yes or no');
         }
-     var favoriteCity= prompt('Do you think Portland is my favorite city ?');
+     var favoriteCity= prompt('Hi ' + userName + ' Do you think Portland is my favorite city ?');
      
-         if( favoriteCity .toUpperCase == 'no'){
-            console.log();
-             alert('yeaaaa its Seattle')
+         if( favoriteCity .toUpperCase() === 'NO'){
+            //console.log('yeaaaa its Seattle ' + userName + ' thanks for visiting my website');
+             alert('yeaaaa its Seattle ' + userName + ' thanks for visiting my website');
          }
+         else if(favoriteCity .toUpperCase() === 'YES'){
+              //console.log('noooo its Seattle. ' + userName + ' thanks for visiting my website');
+             alert('noooo its Seattle. ' + userName + ' thanks for visiting my website');
+         }
+
          else{
-             alert('noooo its Seattle')
-         }
-
-
+            // console.log('please answer yes or no');
+            alert('please answer yes or no');
+        }
 
 
 
