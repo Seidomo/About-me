5a4fba80290b9fd5530a323fa2fe1d9cb7d86907
Seid Mohamed <seid783@gmail.com>
2020-10-27 17:39:11 -0700

    modified

--- js/app.js
+++ js/app.js
@@ -1,3 +1,70 @@
-'use strict';
 
-var likesPizza= prompt('do you like pizza');
+
+
+'use strict'
+
+   var userName= prompt("please enter your name?");
+        //console.log();
+        alert('Greetings  ' + userName);
+
+    var myHeight= prompt('Hi ' + userName + ' how tall do yo think I am? ');
+       // console.log();
+       
+       
+        if(myHeight == 6.3) {
+            alert('yap you got right');
+        }
+        else{
+            alert ('Sorry no I am 6.3');
+        } 
+        
+    var hairColor= prompt('Do you think my hair color is white ?');
+    
+    
+        if( hairColor .toLowerCase == 'yes'){
+            console.log();
+            alert('No sorry its black')
+
+        }
+        else{
+            alert ('You right its not please click ok to continue')
+        }
+
+    var favoriteColor= prompt('Do you think my favorite color is Blue ?');
+    
+        if( favoriteColor .toLowerCase == 'yes'){
+            console.log();
+             alert ('Applause you got it please click ok to continue')
+        }
+        else{
+            alert ('Sorry no its Blue')
+        }
+
+     var favoriteTeam= prompt('Is Seahawks my favorite team ?');
+     
+        if( favoriteTeam .toLowerCase == 'yes'){
+            console.log();
+            alert('GO HAWKS you got it right')
+        }
+        else{
+            alert( 'No i am a SEAHAWKS fan')
+        }
+     var favoriteCity= prompt('Do you think Portland is my favorite city ?');
+     
+         if( favoriteCity .toUpperCase == 'no'){
+            console.log();
+             alert('yeaaaa its Seattle')
+         }
+         else{
+             alert('noooo its Seattle')
+         }
+
+
+
+
+
+
+
+
+
+
